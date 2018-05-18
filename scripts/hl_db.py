a4adab6553326c043c88a6a091d97305d7137224
krugersu <pk69@kruger.su>
2018-05-18 16:59:33 +0700

    SQL

--- scripts/hl_db.py
+++ scripts/hl_db.py
@@ -6,8 +6,17 @@ class All_book(object):
    
     def __init__(self,cur,path_db):
         """Constructor"""
-        conn = sqlite3.connect(path_db) 
-        self.cur = conn.cursor()
+        self.conn = sqlite3.connect(path_db) 
+        self.cur = self.conn.cursor()
+        # book = [(4,'Наименование', '','' ,'' ,'' ,'','' ,'' ,'','' ,'' ,'','' , '','', '', '')]
+        # # book = [('','Наименование', 2, 4, 2, 0,'7/9/2002', 2, 0,'Обложка', 0, 3,100000, 256, 'Описание'
+        # # ,'ISBN', 'Твердая', 1)]
+        # # book = [('Наименование', 'Библиотека', 'Автор', 'Жанр', 'Ебук','ДатаПубликации', 'Серия', 'Поиск','Обложка', 'Номер в серии', 'Издательство','Тираж', 'Количество страниц', 'Описание'
+        # # ,'ISBN', 'ТипОбложки', 'Художник'),
+          
+ 
+        # self.cur.executemany('INSERT INTO book VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', book)
+        # conn.commit()
     def __create_lib(self):
         pass
 
@@ -24,6 +33,25 @@ class All_book(object):
         pass
     def Create_lib(self):
         pass
+
+    def Add_book(self,cursor,bookD):
+        
+        self.cur.execute("SELECT MAX (id_book) from book")
+        id_book =  self.cur.fetchone()[0] + 1
+        bookD[0] = id_book
+        print(bookD[0])
+        #print(book[0])
+
+        # book = [(5,'Наименование', '','' ,'' ,'' ,'','' ,'' ,'','' ,'' ,'','' , '','', '', '')]
+        # book = [('','Наименование', 2, 4, 2, 0,'7/9/2002', 2, 0,'Обложка', 0, 3,100000, 256, 'Описание'
+        # ,'ISBN', 'Твердая', 1)]
+        # book = [('Наименование', 'Библиотека', 'Автор', 'Жанр', 'Ебук','ДатаПубликации', 'Серия', 'Поиск','Обложка', 'Номер в серии', 'Издательство','Тираж', 'Количество страниц', 'Описание'
+        # ,'ISBN', 'ТипОбложки', 'Художник'),
+          
+ 
+        self.cur.executemany('INSERT INTO book VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', bookD)
+        self.conn.commit()
+        
 
 class Paper_book(All_book):
     """Класс для бумажных книг"""
